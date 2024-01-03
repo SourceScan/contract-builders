98de77b090c33ac52c5c1584f1e921cbb6504876
Andrey Gruzdev <agruzdev1@gmail.com>
2024-01-03 18:28:06 +0100

    update builder script

--- utils/rust/build.sh
+++ utils/rust/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ -z "$KEEP_NAMES" ]; then
+  export RUSTFLAGS='-C link-arg=-s'
+else
+  export RUSTFLAGS=''
+fi
 rustup target add wasm32-unknown-unknown
 cargo build --all --target wasm32-unknown-unknown --release
